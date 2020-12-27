8223301408f64e826ebe20986eadf789599d526d
lanthony42 <7217219+lanthony42@users.noreply.github.com>
2020-12-27 16:37:55 -0500

    Changed version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme_file:
 
 setup_args = dict(
     name='gamecord',
-    version='0.0.8',
+    version='0.0.9',
     description='A helper framework for making games in Discord with discord.py.',
     long_description_content_type="text/markdown",
     long_description=README,
@@ -18,7 +18,7 @@ setup_args = dict(
 )
 
 install_requires = [
-    'discord.py>=1.4.1',
+    'discord.py>=1.5.0',
     'python-dotenv'
 ]
 
